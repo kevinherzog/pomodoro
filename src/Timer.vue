2d3588df315746ca49e1560f7b6dfd0621bf8b15
Kevin <quotenslav@gmail.com>
2025-02-17 07:53:18 +0100

    Fixed play/pause not working

--- src/Timer.vue
+++ src/Timer.vue
@@ -6,90 +6,77 @@
 </template>
 
 <script>
-    import {computed, ref} from 'vue'
-    const workTime = ref(10)
-    const chillTime = ref(5)
-    const longChillTime = ref(15)
-    const round = ref(4)
-    export default {
-        data() {
-            return {
-                relaxTime: false,
-                running: false,
-                timerCount: workTime.value,
-                alarm: new Audio('/alarm.mp3'),
-                intervallId: null
-            }
-        },
-        watch: {
-            running(value) {
-                if (value) {
-                    setTimeout(() => {
-                        this.timerCount--;
-                    }, 1000);
-                }
-            },
-
-            timerCount: {
-                handler(value) {
-                    if (value > 0 && this.running) {
-                        setTimeout(() => {
-                            this.timerCount--;
-                            if (value == 1) {
-                                this.timeEndHandler();
-                            }
-                        }, 1000);
-                    }
-                },
-                immediate: true // This ensures the watcher is triggered upon creation
-            }
-
-        },
-            methods: {
-                startTimer() {
-                    this.intervalId = setInterval(() => {
-                        if (this.timerCount > 0) {
-                        this.timerCount--
-                        } else {
-                        clearInterval(this.intervalId)
-                        this.timeEndHandler()
-                        }
-                    }, 1000)
-                },
-                stopTimer(){
-
-                },
-                playpause(){
-                    this.running = !this.running
-                },
-                breakTime(){
-                    this.relaxTime = true
-                    this.running = false
-                    console.log(round)
-                    if (round.value % 4 == 0) { // Check if time for big break
-                        console.log('long breaky time')
-                        this.timerCount = longChillTime.value
-                    }else{
-                        this.timerCount = chillTime.value
-                    }
-                },
-                workyTime(){
-                    this.relaxTime = false;
-                    this.running = false
-                    this.timerCount = workTime.value
-                },
-                timeEndHandler(){
-                    this.alarm.play()
-                    if (this.relaxTime) {
-                        round.value++
-                        this.workyTime()
-                    }else{
-                        this.breakTime()
-                    }
-                },
-                skip(){
-                    this.timeEndHandler()
-                }
-            }
+import { computed, ref } from 'vue'
+const workTime = ref(10)
+const chillTime = ref(5)
+const longChillTime = ref(15)
+const round = ref(4)
+export default {
+    data() {
+        return {
+            relaxTime: false,
+            running: false,
+            timerCount: workTime.value,
+            alarm: new Audio('/alarm.mp3'),
+            intervallId: null
         }
+    },
+    methods: {
+        startTimer() {
+            if (this.intervalId) return; // Prevent duplicate intervals
+
+            this.running = true;
+            this.intervalId = setInterval(() => {
+                if (this.timerCount > 0) {
+                    this.timerCount--;
+                } else {
+                    this.timeEndHandler();
+                    clearInterval(this.intervalId);
+                    this.intervalId = null; // Reset interval ID
+                }
+            }, 1000);
+        },
+
+        stopTimer() {
+            this.running = false;
+            clearInterval(this.intervalId);
+            this.intervalId = null; // Reset interval ID
+        },
+        playpause() {
+            console.log(this.running)
+            if (this.running) {
+                this.stopTimer()
+            }else{
+                this.startTimer()
+            }
+        },
+        breakTime() {
+            this.relaxTime = true
+            this.running = false
+            if (round.value % 4 == 0) { // Check if time for big break
+                console.log('long breaky time')
+                this.timerCount = longChillTime.value
+            } else {
+                this.timerCount = chillTime.value
+            }
+        },
+        workyTime() {
+            this.relaxTime = false;
+            this.running = false
+            this.timerCount = workTime.value
+        },
+        timeEndHandler() {
+            // this.alarm.play()
+            if (this.relaxTime) {
+                round.value++
+                this.workyTime()
+            } else {
+                this.breakTime()
+            }
+        },
+        skip() {
+            this.timeEndHandler()
+        }
+    }
+}
 </script>
